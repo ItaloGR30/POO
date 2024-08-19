42042ecc3efcf95ae46164fff323d90cdf427e7f
ItaloGR30 <r.italo@escolar.ifrn.edu.br>
2024-08-19 09:30:39 -0300

    Primeiro commit
    Primeira questão

---
+++ 1 questão;.cs
@@ -0,0 +1,9 @@
+int numerointeiro = 5;
+float numeroreal = 5.9f;
+string texto = "italo";
+bool fv = false;
+
+Console.WriteLine(numerointeiro);
+Console.WriteLine(numeroreal);
+Console.WriteLine(texto);
+Console.WriteLine(fv);
