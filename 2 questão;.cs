795209226c3dbb389d5bcfd75a5b3535b8e996c1
ItaloGR30 <r.italo@escolar.ifrn.edu.br>
2024-08-19 09:31:28 -0300

    Segundo commit
    Segunda questão

---
+++ 2 questão;.cs
@@ -0,0 +1,12 @@
+int n = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+
+int soma = n+n2;
+int subtracao = n-n2;
+int multiplicacao = n*n2;
+int divisao = n/n2;
+
+Console.WriteLine(soma);
+Console.WriteLine(subtracao);
+Console.WriteLine(multiplicacao);
+Console.WriteLine(divisao);
